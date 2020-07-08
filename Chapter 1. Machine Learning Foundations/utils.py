d89f0380b93de14781f5fb6422faa5e9601d65d3
Harry <harryaberg@gmail.com>
2020-07-08 20:28:42 +0100

    added regression utils and boosting alg works

--- Chapter 1. Machine Learning Foundations/utils.py
+++ Chapter 1. Machine Learning Foundations/utils.py
@@ -7,6 +7,33 @@ def get_classification_data(sd=6, m=10, n_features=2, n_clusters=2):
     X, Y = sklearn.datasets.make_blobs(n_samples=m, n_features=n_features, centers=n_clusters, cluster_std=sd)
     return X, Y
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_regression_data(m=20): 
+    ground_truth_w = 2.3 # slope
+    ground_truth_b = -8 #intercept
+    X = np.random.randn(m, 1)*2
+    Y = ground_truth_w*X + ground_truth_b + 1*np.random.randn(m, 1)
+    print(X.shape, Y.shape)
+    return X, Y #returns X (the input) and Y (labels)
+
+def show_regression_data(X, Y):
+    plt.figure()
+    plt.scatter(X, Y, c='r')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.show()
+
+def visualise_regression_predictions(X, y_hat, Y):
+    plt.figure()
+    plt.scatter(X, Y, c='r', label='Label')
+    plt.scatter(X, y_hat, c='b', label='Hypothesis', marker='x')
+    plt.legend()
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.show()
+
 def calc_accuracy(predictions, labels):
     return np.mean((predictions == labels).astype(int)) * 100
 
